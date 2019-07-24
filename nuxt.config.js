60d92f1b3086475a572969db46c25c81bac80b64
luoliyi <mopln510@qq.com>
2019-07-24 15:04:31 +0800

    init project

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'nuxtdemo',
+    title: 'nuxt.js demo',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
